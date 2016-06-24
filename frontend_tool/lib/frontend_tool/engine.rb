1ada23a1174e1287f545892334a9b29d3e5ed52f
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2016-06-24 10:39:45 +0100

    Copy images from govuk_templat
    - Copy all the images from govuk_template
    - Update asset paths with the scope/prefix for this engine
    
    Requires a path change, because of the engine scoping/directory
    structure - not sure if thats something we'd want to replicate for
    a real package, it's kind of annoying, but the explicitness is
    useful in places.

--- frontend_tool/lib/frontend_tool/engine.rb
+++ frontend_tool/lib/frontend_tool/engine.rb
@@ -10,6 +10,15 @@ module FrontendTool
       %w(
         frontend_tool/govuk-template.css
         frontend_tool/fonts.css
+        frontend_tool/favicon.ico
+        frontend_tool/apple-touch-icon-120x120.png
+        frontend_tool/apple-touch-icon-152x152.png
+        frontend_tool/apple-touch-icon-60x60.png
+        frontend_tool/apple-touch-icon-76x76.png
+        frontend_tool/gov.uk_logotype_crown_invert.png
+        frontend_tool/gov.uk_logotype_crown_invert_trans.png
+        frontend_tool/gov.uk_logotype_crown.svg
+        frontend_tool/opengraph-image.png
       )
     end
   end
