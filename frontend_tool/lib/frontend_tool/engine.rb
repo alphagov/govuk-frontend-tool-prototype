7aabe9bcccb715f4e2e6ee1f3df88d06820758f8
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2016-06-24 09:27:06 +0100

    Copy main stylsheets from govuk_template
    Enough to make the page render more or less the same.
    
    Pretty much straight copied, but with some imports brought inline,
    and the colours from `styleguide` moved to the relevant module.
    
    Added a root CSS file for all template styles, mirrors existing setup
    but might want this to be flexible in the future, part of app sass?
    
    Added a `to-sort` folder to the ITCSS structure, as I was spending too
    long thinking where things should go. Modules like accessibility need
    splitting into many different layers of ITCSS, so skip that for now.
    
    Doesn't include:
    - No IE specific CSS
    - No IE fonts
    - No print

--- frontend_tool/lib/frontend_tool/engine.rb
+++ frontend_tool/lib/frontend_tool/engine.rb
@@ -1,5 +1,16 @@
 module FrontendTool
   class Engine < ::Rails::Engine
     isolate_namespace FrontendTool
+
+    initializer "frontend_tool.assets.precompile" do |app|
+      app.config.assets.precompile += precompile_assets
+    end
+
+    def precompile_assets
+      %w(
+        frontend_tool/govuk-template.css
+        frontend_tool/fonts.css
+      )
+    end
   end
 end
